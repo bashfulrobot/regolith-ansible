48a2091c5908d09d6f08df6cdf8147cf2ed6dbbf
Dustin Krysak <dustin@bashfulrobot.com>
2020-06-11 17:39:30 -0700

    🚀 Added deploy scripts
    - I have a bad memory.
    - Will write a better all-in-one script at a later date.

---
+++ deploy.laptop.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#~/.local/bin/ansible-lint -x 401 run.yml
+
+# read -p "Run play? [Enter] for yes, [Ctrl+c] for no."
+
+sudo ls >/dev/null
+ansible-playbook --skip-tags "desktop" --connection=local run.yml
+
+exit 0
