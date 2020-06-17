e914f8a1b87e4da002270a6e7f2e973a7237006c
Dustin Krysak <dustin@bashfulrobot.com>
2020-06-17 11:45:26 -0700

    🚀 Added cargo install script until I move into ansible.

---
+++ files/helper/cargo-installs.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+cargo install skim
+# Moved to github releases
+# cargo install dust
+cargo install sd
+cargo install ytop
+cargo install bottom
+cargo install exa
+cargo install ripgrep
+cargo install tokei
+cargo install procs
+cargo install fd
+
+# Suggested aliases
+# alias ls='exa'
+# alias find='fd'
+# alias grep='rp'
+# alias du='dust'
+# alias cat='bat'
+# alias cloc='tokei'
+# alias ps='procs'
+# alias sed='sd'
+# alias top='btm'
