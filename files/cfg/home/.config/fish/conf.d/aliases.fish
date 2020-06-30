7a5aa3991e78d473dde6ccca2b70697387f76ad0
Dustin Krysak <dustin@bashfulrobot.com>
2020-06-29 20:28:29 -0700

    🐛 fixed a bashism in my fish file

--- files/cfg/home/.config/fish/conf.d/aliases.fish
+++ files/cfg/home/.config/fish/conf.d/aliases.fish
@@ -26,8 +26,8 @@ alias cfg-update="$HOME/tmp/bashfulrobot-ansible/deploy.sh"
 alias editorconfig-init="cp $HOME/.config/editorconfig/.editorconfig ."
 alias uncrustify-init="cp $HOME/.config/uncrustify/.uncrustify.cfg ."
 
-alias oni2="${HOME}/Documents/bin/Onivim2-x86_64.AppImage"
+alias oni2="$HOME/Documents/bin/Onivim2-x86_64.AppImage"
 
-alias gen-passwd="${HOME}/Documents/bin/generate-passwd.sh"
+alias gen-passwd="$HOME/Documents/bin/generate-passwd.sh"
 
 alias gh-init="/usr/local/bin/github-init-main.sh"
