97a61f0a89d05f4ca370c16cb98ec22f00ebb50d
Dustin Krysak <dustin@bashfulrobot.com>
2020-06-30 16:54:33 -0700

    ✨ Auto install fisher plugins

--- files/cfg/home/.config/fish/config.fish
+++ files/cfg/home/.config/fish/config.fish
@@ -1,1 +1,9 @@
+# auto install new fisherfile plugins
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+# My prompt
 starship init fish | source
