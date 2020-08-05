ca5bf11160adc9a3db0eabc208106af09fa629d1
Dustin Krysak <dustin@bashfulrobot.com>
2020-08-05 13:48:15 -0700

    ♻️ Changed Go paths

--- files/cfg/home/.config/fish/conf.d/env.fish
+++ files/cfg/home/.config/fish/conf.d/env.fish
@@ -2,5 +2,6 @@
 set -gx PATH "$HOME/.cargo/bin" $PATH
 
 # Support Go
-set -x -U GOPATH $HOME/go
-set -x -U GOROOT /snap/go/current
+set -x -U GOPATH "$HOME/go"
+set -x -U GOROOT "/usr/local/go"
+set -gx PATH "/usr/local/go/bin" "$GOPATH/bin" $PATH
