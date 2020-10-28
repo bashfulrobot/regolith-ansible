561c81ffdad1eb4022b9395132cd81c4020d590d
bashfulrobot <dustin@bashfulrobot.com>
2020-10-28 15:28:42 -0700

    ⚡️ Fix jump values for finer adjustment

--- files/cfg/home/.config/fish/functions/cam-bright-down.fish
+++ files/cfg/home/.config/fish/functions/cam-bright-down.fish
@@ -1,5 +1,5 @@
 function cam-bright-down -d "Decrease Camera Brightness"
     set CURRENT_VAL (v4l2-ctl -d /dev/video0 -l | rg brightness | cut -d '=' -f6)
-    set NEW_VAL (math "$CURRENT_VAL-10")
+    set NEW_VAL (math "$CURRENT_VAL-5")
     v4l2-ctl -d /dev/video0 --set-ctrl brightness=$NEW_VAL
 end
--- files/cfg/home/.config/fish/functions/cam-sat-up.fish
+++ files/cfg/home/.config/fish/functions/cam-sat-up.fish
@@ -1,5 +1,5 @@
 function cam-sat-up -d "Increase Camera Saturation"
     set CURRENT_VAL (v4l2-ctl -d /dev/video0 -l | rg saturation | cut -d '=' -f6)
-    set NEW_VAL (math "$CURRENT_VAL+10")
+    set NEW_VAL (math "$CURRENT_VAL+5")
     v4l2-ctl -d /dev/video0 --set-ctrl saturation=$NEW_VAL
 end
