d19fc888fa067e7e3780a7455557fff2784a2971
bashfulrobot <dustin@bashfulrobot.com>
2020-11-27 18:49:00 -0800

    🎨  Added single spot to update camera source

---
+++ files/cfg/home/.config/fish/functions/br.fish
@@ -0,0 +1,5 @@
+function br -d "Increase Camera Brightness"
+    set CURRENT_VAL (v4l2-ctl -d /dev/video0 -l | rg brightness | cut -d '=' -f6)
+    set NEW_VAL (math "$CURRENT_VAL+10")
+    v4l2-ctl -d /dev/video0 --set-ctrl brightness=$NEW_VAL
+end
